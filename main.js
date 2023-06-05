593a8b8c061c46a226b7e770f403eef154514e06
long252725 <inthanhlong2006@gmail.com>
2023-06-05 12:14:33 +0700

    web-thu-hai

---
+++ main.js
@@ -0,0 +1,8 @@
+function getNumber(){
+    input = document.getElementById("cn")
+    myNumber = input.value
+    document.getElementById("card_numb").innerHTML = myNumber
+}
+let text = "Please locate where 'locate' occurs!";
+let index = text.indexOf("locate");
+document.getElementById("demo").innerHTML = "hhhggg"; 
